5fd23d36b4365358279936380f0d17a52a1c943a
FSharp4 <jakepeterson163@gmail.com>
2020-02-10 03:31:19 -0500

    Revert "Revert "Documentation, deletion of explicit INCLUSION_MARGIN""
    This reverts commit 45e724ed0d118bda91e77defd7d5d44384cbd949.

--- heartDrawer/src/com/fsharp/heartDrawer/HeartTester.java
+++ heartDrawer/src/com/fsharp/heartDrawer/HeartTester.java
@@ -4,13 +4,20 @@
 package com.fsharp.heartDrawer;
 
 /**
+ * HeartTester.java Tests the HeartDrawer class.
+ * 
  * @author FSharp4
- *
  */
 public class HeartTester {
 
+  /**
+   * Main entry point. Mostly for in-IDE testing. Included for convenience. Draws a heart with
+   * radius 14 with default filename.
+   * 
+   * @param args Unused.
+   */
   public static void main(String[] args) {
-    //test heart
+    // test heart
     String[] testArgs = {"14"};
     HeartDrawer.main(testArgs);
   }
